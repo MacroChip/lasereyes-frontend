76b60aaca2671b1fabfe8f282ac6710d20fb5280
Chip Thien <themacrochip@gmail.com>
2021-06-15 18:42:00 -0500

    Making a change to trigger redeploy

--- src/main.js
+++ src/main.js
@@ -10,5 +10,14 @@ const app = new App({
 
 window.app = app;
 window.firebase.analytics();
+// var firebaseConfig = {
+// 	apiKey: "AIzaSyBM_05ysmvXdTiX9LdYhlkj4PTx2mFW8OA",
+// 	authDomain: "auto-laser.firebaseapp.com",
+// 	projectId: "auto-laser",
+// 	storageBucket: "auto-laser.appspot.com",
+// 	messagingSenderId: "940020933136",
+// 	appId: "1:940020933136:web:9d6e3b09509c430dae6095",
+// 	measurementId: "G-TPN1C8PYJJ"
+//   };
 
 export default app;
