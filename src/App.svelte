f2d42fae0034dd1382a7b4bf7059c7cd70e3b399
Chip Thien <themacrochip@gmail.com>
2021-06-16 21:48:14 -0500

    Add some instructions

--- src/App.svelte
+++ src/App.svelte
@@ -64,10 +64,26 @@
         display: block;
         margin: 0 auto;
     }
+
+    .upload-label {
+        border: 1px solid #00bebe;
+        display: inline-block;
+        padding: 6px 12px;
+        cursor: pointer;
+    }
+    .upload-label:hover {
+        box-shadow: 0 0 25px #00dcdc, 0 1px 5px #212121;
+        border-color: #00dcdc;
+        background:  rgba(0, 190, 190, 0.2);
+    }
 </style>
 
+<h1>Automatic🧠 laser⚡ eyes👀</h1>
 <form>
-    <input type="file" name="image" id="image">
+    <label for="image" class="upload-label">
+        Click here to choose an image
+    </label>
+    <input type="file" name="image" id="image" style="display: none;">
     <button on:click={search}>Laserify</button>
 </form>
 {#if laserPromise}
@@ -80,3 +96,4 @@
     <div>{err}</div>
   {/await}
 {/if}
+<a href="https://twitter.com/themacrochip" target="_blank">Made by Chip Thien</a>
